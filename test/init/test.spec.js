4239aa57e5aef81b1a83d9a271bb642748c85d87
Okocha Moses <okocha.chukwutem@gmail.com>
2020-02-11 08:56:20 +0100

    add circleCI config and mocha test configuration

---
+++ test/init/test.spec.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+var expect = require("chai").expect;
+var should = require("chai").should();
+
+describe("Test initialization", () => {
+  it("1 + 1 to equal 2", function() {
+    expect(1 + 1).to.equal(2);
+  });
+});
